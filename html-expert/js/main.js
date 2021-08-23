406702d73ff3dcd61796715741bff48dcbf805b0
soshirokov <soshirokov@gmail.com>
2021-08-23 17:36:43 +0300

    + адаптив страницы Product

--- html-expert/js/main.js
+++ html-expert/js/main.js
@@ -12,4 +12,18 @@ $(function () {
 
     $("#priceStart").val("$" + $("#slider-range").slider("values", 0));
     $("#priceEnd").val("$" + $("#slider-range").slider("values", 1));
+
+    $("#slider-range-mobile").slider({
+        range: true,
+        min: 0,
+        max: 1000,
+        values: [0, 1000],
+        slide: function (event, ui) {
+            $('#priceStart-mobile').val("$" + ui.values[0]);
+            $('#priceEnd-mobile').val("$" + ui.values[1]);
+        }
+    });
+
+    $("#priceStart-mobile").val("$" + $("#slider-range").slider("values", 0));
+    $("#priceEnd-mobile").val("$" + $("#slider-range").slider("values", 1));
 });
